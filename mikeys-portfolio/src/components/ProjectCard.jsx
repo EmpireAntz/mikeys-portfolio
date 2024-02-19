6efce6c3e417a5c306829f0c6135d665b5675e7b
EmpireAntz <thebigboognish@gmail.com>
2024-02-18 18:46:00 -0800

    added cards and ways to link those cards to projects. i just need to add the projects and style the cards better and then add a resume.

---
+++ mikeys-portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ProjectCard({ title, imageUrl, projectUrl }) {
+  return (
+    <div className="card bg-dark text-white">
+      <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+        <img src={imageUrl} className="card-img" alt={title} />
+        <div className="card-img-overlay d-flex align-items-end p-0">
+          <h5 className="text-white w-100 text-center bg-dark bg-opacity-75 py-2">{title}</h5>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- mikeys-portfolio/src/pages/Portfolio.jsx
+++ mikeys-portfolio/src/pages/Portfolio.jsx
@@ -1,5 +1,32 @@
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+
 function Portfolio() {
-    return <div>Portfolio</div>;
-  }
-  
-  export default Portfolio;
+  const projects = [
+    {
+      title: 'Convo',
+      imageUrl: '',
+      projectUrl: 'http://link_to_project_1.com'
+    },
+    {
+        title: 'Book Looker',
+        imageUrl: '',
+        projectUrl: 'http://link_to_project_1.com'
+    },
+  ];
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center mb-4">Portfolio</h2>
+      <div className="row g-4">
+        {projects.map((project, index) => (
+          <div className="col-md-6" key={index}>
+            <ProjectCard {...project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
